35f8e482c16b079a948e4991663238569eaef9d0
MaximoS-py <dawesterf@gmail.com>
2026-09-03 13:12:34 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 SECRET_KEY = 'nejake-tajne-heslo'
 UPLOAD_FOLDER = 'static/uploads'
 
-MAIL_SERVER = 'smtp.seznam.cz'
+MAIL_SERVER = '://brevo.com'
 MAIL_PORT = 587
 MAIL_USE_TLS = True
 MAIL_USE_SSL = False
 MAIL_USERNAME = 'rekonstrukceholy@seznam.cz'     # vaše celá adresa
 MAIL_PASSWORD = 'HolySteoan777333.'   # speciální heslo vygenerované v Seznamu
+MAIL_SERVER = '://brevo.com'
+
