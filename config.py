df2662fb5a974e0cd0d5add948ba2508b132e470
MaximoS-py <dawesterf@gmail.com>
2026-09-04 10:58:02 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,9 @@
 import os
 
-SECRET_KEY = 'nejake-tajne-heslo'
-UPLOAD_FOLDER = 'static/uploads'
+SECRET_KEY = os.environ.get("SECRET_KEY", "tajny_klic_pro_faktury")
+UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), "static", "uploads")
 
-# Tajné přihlašovací údaje načítané ze serveru
-ADMIN_USERNAME = os.environ.get('WEB_USERNAME', 'admin')
-ADMIN_PASSWORD = os.environ.get('WEB_PASSWORD', 'admin123')
-
-
-MAIL_SERVER = '://brevo.com'
-MAIL_PORT = 587
-MAIL_USE_TLS = True
-MAIL_USE_SSL = False
-MAIL_USERNAME = 'rekonstrukceholy@seznam.cz'     # vaše celá adresa
-MAIL_PASSWORD = 'HolySteoan777333.'   # speciální heslo vygenerované v Seznamu
-MAIL_SERVER = '://brevo.com'
-
+SMTP_SERVER = os.environ.get("SMTP_SERVER", "smtp.example.com")
+SMTP_PORT = int(os.environ.get("SMTP_PORT", "587"))
+SMTP_UZIVATEL = os.environ.get("SMTP_UZIVATEL", "noreply@example.com")
+SMTP_HESLO = os.environ.get("SMTP_HESLO", "heslo")
